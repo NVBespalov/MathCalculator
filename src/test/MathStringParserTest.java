375ad37e6c6a8ccaaf0b967994d2ffc9d526f019
Nikolay <nbespalov@icloud.com>
2015-04-30 23:42:44 +0500

    red test for constructor return type

---
+++ src/test/MathStringParserTest.java
@@ -0,0 +1,30 @@
+package test;
+
+import com.iSoft.calculator.BaseParser;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import java.util.Stack;
+import static org.hamcrest.CoreMatchers.instanceOf;
+
+
+public class MathStringParserTest extends Assert {
+
+    private BaseParser sut;
+    @Before
+    public void setUp() throws Exception {
+        this.sut = new BaseParser();
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        this.sut = null;
+    }
+
+    @Test
+    public void testParse() {
+        assertThat(this.sut.parse(""), instanceOf(Stack.class));
+    }
+
+}
