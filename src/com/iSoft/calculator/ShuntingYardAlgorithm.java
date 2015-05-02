073a79d84efd6600eb6e4ae081b9a6731b750bc4
Nikolay <nbespalov@icloud.com>
2015-05-03 01:48:48 +0500

    Shunting-yard algorithm backbone

---
+++ src/com/iSoft/calculator/ShuntingYardAlgorithm.java
@@ -0,0 +1,39 @@
+package com.iSoft.calculator;
+
+import java.util.Objects;
+import java.util.Stack;
+import java.util.StringTokenizer;
+
+/**
+ * Algorithm to convert string from infix to postfix notation
+ */
+public class ShuntingYardAlgorithm {
+
+    /**
+     * Stack to hold operators
+     */
+    protected Stack<String> operatorStack;
+
+    /**
+     * Result postfix string builder chain
+     */
+    private StringBuilder postfixStringBuilder;
+
+    /**
+     * Parse string from infix to postfix notation
+     *
+     * @param stringToConvert String in infixNotation
+     * @return String in postfixNotation
+     */
+    public String execute(String stringToConvert) {
+
+        String currentToken;
+        operatorStack = new Stack<>();
+        StringTokenizer parser = new StringTokenizer(stringToConvert, "+-*/^() ", true);
+        postfixStringBuilder = new StringBuilder(stringToConvert.length());
+
+        return postfixStringBuilder.toString();
+    }
+
+
+}
