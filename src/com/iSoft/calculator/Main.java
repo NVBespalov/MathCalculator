4ef4d08c78d56bd1a4d2f679bc67fec89e390a16
Nikolay <nbespalov@icloud.com>
2015-05-03 02:57:31 +0500

    prompt input

--- src/com/iSoft/calculator/Main.java
+++ src/com/iSoft/calculator/Main.java
@@ -1,7 +1,23 @@
 package com.iSoft.calculator;
 
+import com.iSoft.calculator.converter.IConverter;
+import com.iSoft.calculator.converter.InfixToPostfixConverter;
+import com.iSoft.calculator.converter.ShuntingYardAlgorithm;
+import com.iSoft.calculator.evaluator.IEvaluator;
+import com.iSoft.calculator.evaluator.PostfixEvaluator;
+
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
+        do {
+            Scanner reader = new Scanner(System.in);
+            System.out.println("\nВведите выражение: ");
+            String infixString = reader.next();
+            IConverter converter = new InfixToPostfixConverter(new ShuntingYardAlgorithm());
+            IEvaluator evaluator = new PostfixEvaluator();
+            System.out.print("Результат: " + evaluator.evaluate(converter.convert(infixString)) + "\n");
+        } while (true);
     }
 }
